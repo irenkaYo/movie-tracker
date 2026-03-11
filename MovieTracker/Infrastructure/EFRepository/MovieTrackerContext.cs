1a4e2aaa54c899dcf5af9fdab59daad57e6686fe
irenaYo <irenatugushi997@gmail.com>
2026-03-11 15:00:39 +0200

    create MovieTrackerContext

---
+++ MovieTracker/Infrastructure/EFRepository/MovieTrackerContext.cs
@@ -0,0 +1,26 @@
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace Infrastructure.EFRepository;
+
+public class MovieTrackerContext : DbContext
+{
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<MovieActor> MovieActors { get; set; }
+    public MovieTrackerContext() 
+        : base()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .Build();
+ 
+        optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+    }
+}
