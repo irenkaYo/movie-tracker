e893e00df0845080050145b065422c6f7a9af866
irenaYo <irenatugushi997@gmail.com>
2026-03-22 12:37:21 +0200

    add constructor to Movie.cs

--- MovieTracker/Domain/Models/Movie.cs
+++ MovieTracker/Domain/Models/Movie.cs
@@ -12,7 +12,19 @@ public class Movie
     public Guid GenreId { get; set; }
     public List<MovieActor> MovieActors { get; set; }
 
-    public Movie(string title, int year, int durationMinutes, Guid genreId)
+    public Movie(string title, int year, int durationMinutes, Genre genre, Guid genreId)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Year = year;
+        DurationMinutes = durationMinutes;
+        IsWatched = false;
+        Genre = genre;
+        GenreId = genreId;
+        MovieActors = new List<MovieActor>();
+    }
+    
+    private Movie(string title, int year, int durationMinutes, Guid genreId)
     {
         Id = Guid.NewGuid();
         Title = title;
@@ -20,5 +32,6 @@ public class Movie
         DurationMinutes = durationMinutes;
         IsWatched = false;
         GenreId = genreId;
+        MovieActors = new List<MovieActor>();
     }
 }
